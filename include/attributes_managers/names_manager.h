ed58f3bc0b874794504b6c2a1411c7b157859bce
ido.dotan <ido682@gmail.com>
2020-03-01 03:21:05 +0200

    all the required working

--- include/attributes_managers/names_manager.h
+++ include/attributes_managers/names_manager.h
@@ -18,10 +18,14 @@ public:
     NamesManager& operator=(const NamesManager& other) = delete;
 
     void AddUser(full_name_t full_name, user_uid_t user);
-    users_ordered_cont_t GetUsersByName(const std::string& name);
+    users_ordered_cont_t GetUsersByName(const name_t& name);
     void DeleteUser(full_name_t full_name, user_uid_t uuid);
 
 private:
     UUIDsMapper m_first_names;
     UUIDsMapper m_last_names;
+
+    users_ordered_cont_t get_users_by_name_with_one_word(const name_t& name);
+    users_ordered_cont_t get_users_by_name_with_two_words(const name_t& name);
+
 }; //NamesManager
